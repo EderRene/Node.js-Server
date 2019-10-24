bcbfbf3e44d00002712b1808802495bf0b11ed61
ThomasSalcher <salcher25@gmail.com>
2019-10-24 20:00:14 +0200

    Node initatial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const port = 3000;
+
+const express = require('express');
+const app=express();
+
+
+
+app.listen(port, function () {
+    console.log('Chat API is up and running on port ' + port + '.');
+  });
