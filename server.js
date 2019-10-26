9bb1c8ddd099aa9398b56783f21d28083171995d
HohenwarterPhilipp <phohenw@gmail.com>
2019-10-26 17:51:35 +0200

    basics of server

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const port = 3000;
 const express = require('express');
 const app=express();
 
-console.log('test');
-
 app.listen(port, function () {
     console.log('Chat API is up and running on port ' + port + '.');
-  });
+});
