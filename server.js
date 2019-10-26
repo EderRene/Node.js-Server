9a7e2b02def653ec237a3dd26065cb893e076ca2
HohenwarterPhilipp <phohenw@gmail.com>
2019-10-26 10:42:08 +0200

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 const express = require('express');
 const app=express();
 
-
+console.log('test');
 
 app.listen(port, function () {
     console.log('Chat API is up and running on port ' + port + '.');
